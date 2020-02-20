08d5728c2a6de37498b1c84616e42b46a031bc40
Tabalbar <35385849+Tabalbar@users.noreply.github.com>
2020-02-20 00:04:25 -1000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
 // eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App />, document.getElementById('root'));
 
