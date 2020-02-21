6a4ee19f9cf211df2dd4be90feb10e6ed8b4d914
William Post <wpost@hawaii.edu>
2020-02-21 12:29:22 -1000

    removed func calls from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import MainCalendarPage from './pages/MainCalendarPage';
-import { getAllEvents } from './api/events';
 
 function App() {
-  getAllEvents().then((msg) => console.log(msg));
   return (
     <MainCalendarPage />
   );
