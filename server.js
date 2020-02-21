f1ce1074868544249249fc4b3570fb833bf3ef9d
William Post <wpost@hawaii.edu>
2020-02-20 21:43:44 -1000

    added getAllEvents function for client code

--- server.js
+++ server.js
@@ -6,13 +6,19 @@
 const express = require('express');
 require('dotenv').config(); // allows importing .env file constants into env variables
 require('./db');
+const cors = require('cors');
 const apiRouter = require('./routes/api');
 
-const { SERVER_PORT } = process.env; // env variables
+
+const { SERVER_PORT, NODE_ENV } = process.env; // env variables
 
 const app = express();
 
 app.use(express.json()); // middleware for POSTing json data
+
+if (NODE_ENV === 'development') {
+  app.use(cors());
+}
 
 // bind routes to app
 app.use(apiRouter);
