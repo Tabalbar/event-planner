88a0d81785c7b534fe57c0c74b9b5fc59a62288c
Tabalbar <35385849+Tabalbar@users.noreply.github.com>
2020-02-22 22:15:44 -1000

    Revert "Translate most of semantic ui -> react"
    This reverts commit 0d5ffca811441050abd7110ac189dc6116d1472e.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import MainCalendarPage from './pages/MainCalendarPage';
-import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <>
-      <NavBar />
-
-      <MainCalendarPage />
-    </>
+    <MainCalendarPage />
   );
 }
 
--- src/pages/MainCalendarPage.jsx
+++ src/pages/MainCalendarPage.jsx
@@ -1,52 +1,15 @@
 import React from 'react';
-import {
-  Grid, Icon, Button, Header,
-} from 'semantic-ui-react';
 import ComponentExample from '../components/ComponentExample';
-
 
 function MainCalendarPage() {
   return (
     <>
-      <Grid>
-        <Grid.Row>
-          <Grid.Column width={3}>
-          </Grid.Column>
-          <Grid.Column width={9}>
-            <Button>
-              <Icon name="angle left icon" />
-            </Button>
-            <Button>
-              Today
-            </Button>
-            <Button>
-              <Icon name="angle right icon" />
-            </Button>
-          </Grid.Column>
-          <Grid.Column width={4}>
-            <Header>
-              February 2020
-            </Header>
-          </Grid.Column>
-        </Grid.Row>
-
-        <Grid.Row>
-          <Grid.Column width={3}>
-            <Button>
-              <Icon name="add" />
-            </Button>
-          </Grid.Column>
-          <Grid.Column width={13}>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-      <h1>This is part of a eeeepage component</h1>
+      <h1>This is part of a page component</h1>
       <ComponentExample componentNumber={1} />
       <ComponentExample componentNumber={2} />
       <ComponentExample componentNumber={3} />
     </>
   );
 }
-
 
 export default MainCalendarPage;
